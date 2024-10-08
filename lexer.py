4490ead4f66082c77625b917f22cb8ad0af779d1
Tyrone Vargas <77775585+tyroneley@users.noreply.github.com>
2024-10-08 11:43:01 +0700

    upload first commit

---
+++ lexer.py
@@ -0,0 +1,44 @@
+import ply.lex as lex
+
+tokens = (
+    'NUMBER',
+    'PLUS',
+    'MINUS',
+    'TIMES',
+    'DIVIDE',
+    'LPAREN',
+    'RPAREN',
+)
+
+t_PLUS    = r'\+'
+t_MINUS   = r'-'
+t_TIMES   = r'\*'
+t_DIVIDE  = r'/'
+t_LPAREN  = r'\('
+t_RPAREN  = r'\)'
+
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)    
+    return t
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+t_ignore  = ' \t'
+
+def t_error(t):
+    print(f"Illegal character '{t.value[0]}'")
+    t.lexer.skip(1)
+
+lexer = lex.lex()
+
+data = '3 + 4 * ( 10 - 5 ) / 2'
+lexer.input(data)
+
+while True:
+    tok = lexer.token()
+    if not tok:
+        break    # No more input
+    print(tok)
